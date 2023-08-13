174a996e0ce66ea74008b93721cb7682a1f21bfd
captainchicken2023 <captainchicken2023@gmail.com>
2023-08-13 00:21:15 -0500

    HW_Update

--- Employee SQL/Challenge_9_Tables.sql
+++ Employee SQL/Challenge_9_Tables.sql
@@ -1,43 +1,43 @@
--- CREATE TABLE titles(
--- 	title_id VARCHAR NOT NULL PRIMARY KEY,
--- 	title VARCHAR NOT NULL
--- );
+CREATE TABLE titles(
+	title_id VARCHAR NOT NULL PRIMARY KEY,
+	title VARCHAR NOT NULL
+);
 
--- CREATE TABLE employees(
--- 	emp_no INTEGER NOT NULL PRIMARY KEY,
--- 	emp_title_id VARCHAR NOT NULL,
--- 	birth_date DATE NOT NULL,
--- 	first_name VARCHAR NOT NULL,
--- 	last_name VARCHAR NOT NULL,
--- 	sex CHAR NOT NULL,
--- 	hire_date DATE NOT NULL,
--- 	FOREIGN KEY (emp_title_iD) REFERENCES titles(title_id)
--- );
+CREATE TABLE employees(
+	emp_no INTEGER NOT NULL PRIMARY KEY,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex CHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_iD) REFERENCES titles(title_id)
+);
 
--- CREATE TABLE salaries(
--- 	emp_no INTEGER NOT NULL PRIMARY KEY, 
--- 	salary INTEGER NOT NULL,
--- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
--- );
+CREATE TABLE salaries(
+	emp_no INTEGER NOT NULL PRIMARY KEY, 
+	salary INTEGER NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
 
--- CREATE TABLE dept_emp(
--- 	emp_no INTEGER NOT NULL, 
--- 	dept_no VARCHAR NOT NULL,
--- 	PRIMARY KEY (emp_no, dept_no),
--- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
--- );
+CREATE TABLE dept_emp(
+	emp_no INTEGER NOT NULL, 
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
 
--- CREATE TABLE departments(
--- 	"dept_no" VARCHAR NOT NULL PRIMARY KEY, 
--- 	"dept_name" VARCHAR NOT NULL
--- );
+CREATE TABLE departments(
+	"dept_no" VARCHAR NOT NULL PRIMARY KEY, 
+	"dept_name" VARCHAR NOT NULL
+);
 
--- CREATE TABLE dept_manager(
--- 	dept_no VARCHAR NOT NULL,
--- 	emp_no INTEGER NOT NULL PRIMARY KEY,
--- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
--- 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
--- );
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INTEGER NOT NULL PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
 
 ALTER TABLE titles
 ALTER COLUMN title TYPE VARCHAR(50) USING title::VARCHAR(50);
