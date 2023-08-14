f13df7d05c07b0453832f742776643efa0a3c2b7
captainchicken2023 <captainchicken2023@gmail.com>
2023-08-13 23:07:16 -0500

    HW_update

--- Employee SQL/Challenge_9_Queries.sql
+++ Employee SQL/Challenge_9_Queries.sql
@@ -29,7 +29,15 @@ JOIN departments ON dept_manager.dept_no=departments.dept_no
 JOIN employees ON dept_manager.emp_no=employees.emp_no;
 
 -- 4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
-
+SELECT 
+	dept_emp.dept_no,
+	dept_emp.emp_no, 
+	employees.last_name,
+	employees.first_name, 
+	departments.dept_name
+FROM dept_emp
+JOIN employees ON dept_emp.emp_no=employees.emp_no
+JOIN departments ON dept_emp.dept_no=departments.dept_no;
 
 -- 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
